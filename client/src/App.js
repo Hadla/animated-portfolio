0d78026fb5b532266bced36818c58c484ed8f5f1
Hadla Bergman <hadla.bergman@appliedtechnology.se>
2020-08-25 15:21:43 +0200

    Added GSAP animation to element.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './style/startPage.css';
 import './style/mainPage.css';
 
+import { TweenMax, Power4 } from 'gsap';
+
 function App() {
+  let hideText = useRef(null);
+
+  useEffect(() => {
+    const hideText = document.querySelectorAll('#hide--text');
+    TweenMax.from(hideText, 1.5, { y: '100%', ease: Power4.easeOut });
+  });
+
   return (
     <div className='App'>
       <div className='intro'>
@@ -12,13 +21,19 @@ function App() {
 
       <div className='hidden-element'>
         <h1 className='big-text-name'>
-          <span className='hide-text'>Hadla</span>
+          <span className='hide-text' id='hide--text'>
+            Hadla
+          </span>
         </h1>
         <h1 className='big-text-name'>
-          <span className='hide-text'>Bergman</span>
+          <span className='hide-text' id='hide--text'>
+            Bergman
+          </span>
         </h1>
         <h1 className='big-text-name'>
-          <span className='hide-text'>Fredrikson</span>
+          <span className='hide-text' id='hide--text'>
+            Fredrikson
+          </span>
         </h1>
       </div>
     </div>
---
+++ client/src/main.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+// tl = new TimelineMax({});
+
+// tl.from('.hide-text', 1.5, { y: '100%', ease: Power4.easeOut });
