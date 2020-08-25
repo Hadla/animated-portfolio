fbdb539ba1d4cea5a9f8dcf4777b7a998419df88
Hadla Bergman <hadla.bergman@appliedtechnology.se>
2020-08-25 14:16:35 +0200

    Styling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import './App.css';
+import './style/startPage.css';
+import './style/mainPage.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <div className='intro'>
+        <p className='intro-text'>Let's animate</p>
+        <button className='enter-portfolio-btn'>Click me!</button>
+      </div>
+
+      <div className='hidden-element'>
+        <h1 className='big-text-name'>
+          <span className='hide-text'>Hadla</span>
+        </h1>
+        <h1 className='big-text-name'>
+          <span className='hide-text'>Bergman</span>
+        </h1>
+        <h1 className='big-text-name'>
+          <span className='hide-text'>Fredrikson</span>
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
