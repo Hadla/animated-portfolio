c104b9efbac241e5eb461f19d2e0fd9cee9b93ae
Hadla Bergman <hadla.bergman@appliedtechnology.se>
2020-08-26 11:14:36 +0200

    Created and animated menu

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,8 @@
 import React, { useRef, useEffect, Component } from 'react';
 import '../style/homePage.css';
+import profileImage from '../images/profile-image.JPG';
 
-import { TweenMax, Power4, TimelineMax } from 'gsap';
+import { TweenMax, Power4, Back, TimelineMax } from 'gsap';
 import { _renderComplexString } from 'gsap/gsap-core';
 
 function Home() {
@@ -9,13 +10,15 @@ function Home() {
   let tl = new TimelineMax({ paused: true });
 
   useEffect(() => {
-    const hideText = document.querySelectorAll('#hide--text');
+    const hideName = document.querySelectorAll('#hide--name');
     const intro = document.querySelectorAll('.intro');
-    const projectBtn = document.querySelectorAll('.project-btn');
-    // This tween reveals the name of #hide--text
-    tl.staggerFrom(hideText, 1.2, { y: '100%', ease: Power4.easeOut }, 0.25);
-    tl.from(projectBtn, 1.2, { x: '300%', ease: Power4.easeOut });
+    const hideMenuItem = document.querySelectorAll('.hide-menu-item');
+    const hideMenuBtns = document.querySelectorAll('.menu-btns');
     tl.to(intro, 0.3, { opacity: 0 });
+
+    tl.staggerFrom(hideName, 1.2, { y: '100%', ease: Power4.easeOut }, 0.25)
+      .staggerFrom(hideMenuBtns, 1.2, { opacity: 0, duration: 1, y: 50 }, 0.25)
+      .staggerFrom(hideMenuItem, 1.5, { opacity: 0, duration: 1 }, 0.2);
   });
 
   return (
@@ -26,33 +29,47 @@ function Home() {
       }}
     >
       <div className='intro'>
-        <p className='intro-text'>Let's animate</p>
+        {/* <img className='profile-image' src={profileImage}></img> */}
+        <p className='intro-text'>Welcome!</p>
         <button
           className='enter-portfolio-btn'
           onClick={() => {
             tl.play();
           }}
         >
-          Click me!
+          Enter
         </button>
       </div>
-      <div className='hidden-element'>
-        <h1 className='big-text-name'>
-          <span className='hide-text' id='hide--text'>
-            Hadla <button className='project-btn'>Projects</button>
-          </span>
-        </h1>
+      <div className='hidden-elements'>
+        <div className='hidden-element'>
+          <h1 className='name-item'>
+            <span className='hide-name' id='hide--name'>
+              Hadla
+            </span>
+          </h1>
 
-        <h1 className='big-text-name'>
-          <span className='hide-text' id='hide--text'>
-            Bergman
-          </span>
-        </h1>
-        <h1 className='big-text-name'>
-          <span className='hide-text' id='hide--text'>
-            Fredrikson
-          </span>
-        </h1>
+          <h1 className='name-item'>
+            <span className='hide-name' id='hide--name'>
+              Bergman
+            </span>
+          </h1>
+          <h1 className='name-item'>
+            <span className='hide-name' id='hide--name'>
+              Fredrikson
+            </span>
+          </h1>
+        </div>
+        <div className='hidden-element menu-btns'>
+          <p className='menu-item'>
+            <span className='hide-menu-item'>About</span>
+          </p>
+          <p className='menu-item'>
+            <span className='hide-menu-item'>Projects</span>
+          </p>
+          <p className='menu-item'>
+            <span className='hide-menu-item'>Contact</span>
+          </p>
+        </div>
       </div>
     </div>
   );
